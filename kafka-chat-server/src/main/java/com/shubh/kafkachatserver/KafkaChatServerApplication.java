27700e54efcec16a988f1487ec37da1916aaa310
Raluca Carare <carare.raluca27@gmail.com>
2021-11-10 15:28:14 +0200

    Added SpringBoot project

---
+++ kafka-chat-server/src/main/java/com/shubh/kafkachatserver/KafkaChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.shubh.kafkachatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaChatServerApplication.class, args);
+	}
+
+}
