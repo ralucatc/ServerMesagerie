4bf294d2fb5f1272944809c626f933f0608bac83
Denisa Pap <papdeni11@gmail.com>
2021-12-18 16:28:47 +0200

    Added MessageConsumer class

---
+++ MessageServer/src/MessageConsumer.java
@@ -0,0 +1,38 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Queue;
+
+public class MessageConsumer extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Message consumer started...");
+
+        //TODO Add Server class
+        while (true) {
+            try {
+                Queue<Message> messages = Server.getMessages();
+                Message message = messages.poll();
+                Server.setMessages(messages);
+
+                if (message != null) {
+                    consumeMessage(message);
+                }
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void consumeMessage(Message message) {
+        try {
+            User destinationUser = message.getDestinationUser();
+            DataOutputStream outputStream = new DataOutputStream(Server.getConnectedClients().get(destinationUser).getOutputStream());
+            outputStream.writeUTF(message.toString());
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
